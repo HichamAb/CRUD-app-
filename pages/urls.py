c38d0cc985cfe1426a1071e7a8ba8d4519bfaeb2
Hicham Abdealtif <subzer0crack@gmail.com>
2020-02-11 13:00:39 +0100

    added the path of specific post with slug pattren

--- pages/urls.py
+++ pages/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import HomePage
+from .views import HomePage,PostDetailView
 
 urlpatterns = [
     
     path('',HomePage.as_view(),name="home-page"),
+    path('<slug:slug>/',PostDetailView.as_view(),name="post-detail"),
 
 ]
