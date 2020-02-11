83c020afa78509775860efed7e457c96c5ce8d53
Hicham Abdealtif <subzer0crack@gmail.com>
2020-02-11 12:59:33 +0100

    added PostDetailView to views

--- pages/views.py
+++ pages/views.py
@@ -1,10 +1,16 @@
 from django.shortcuts import render
 
 from .models import Post
-from django.views.generic import ListView 
+from django.views.generic import ListView ,DetailView
 class HomePage(ListView):
     queryset = Post.objects.all()
     template_name = "pages/posts.html"
     
     context_object_name = "posts"
 
+
+class PostDetailView(DetailView):
+    model = Post
+    context_object_name = 'post'
+    slug_url_kwarg = 'slug'
+    template_name = "pages/post.html"
