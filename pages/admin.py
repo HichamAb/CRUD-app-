0c1fb5d510f792da5c9c3b1a61002412968e5924
Hicham Abdealtif <subzer0crack@gmail.com>
2020-02-13 00:59:30 +0100

    edited the admin area in the post model

--- pages/admin.py
+++ pages/admin.py
@@ -3,4 +3,20 @@ from django.contrib import admin
 # Register your models here.
 from .models import Post 
 
-admin.site.register(Post)
+
+
+class PostAdmin(admin.ModelAdmin) : 
+    list_display = ["title","author","created_on","status","slug"]
+    
+    fieldsets = (
+        ("Post Info", {
+            'fields': (("title","author","slug"),"content")
+                  
+        }),
+        ('Date && Status' , {
+            'fields':("created_on","status")
+        }),
+    )
+    
+
+admin.site.register(Post,PostAdmin)
