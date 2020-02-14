fe109fe423099904ed9ca16e657bdf6bb01225fe
Hicham Abdealtif <subzer0crack@gmail.com>
2020-02-14 20:44:52 +0100

    edited the view file added NewPost View

--- pages/views.py
+++ pages/views.py
@@ -1,7 +1,9 @@
 from django.shortcuts import render
-
+from django.contrib.auth.mixins import LoginRequiredMixin,UserPassesTestMixin
 from .models import Post
-from django.views.generic import ListView ,DetailView
+from django.views.generic import ListView ,DetailView , TemplateView,CreateView
+from django.urls import reverse
+
 class HomePage(ListView):
     queryset = Post.objects.all()
     template_name = "pages/posts.html"
@@ -9,8 +11,16 @@ class HomePage(ListView):
     context_object_name = "posts"
 
 
+class NewPostCreateView(LoginRequiredMixin,CreateView) : 
+    model = Post
+    template_name = "pages/post_new.html"  
+    fields = ['title','content','status','created_on']
+    
+
 class PostDetailView(DetailView):
     model = Post
     context_object_name = 'post'
     slug_url_kwarg = 'slug'
     template_name = "pages/post.html"
+
+    
