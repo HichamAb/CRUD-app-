f4665f520a3d18fb4f0fe3b88de064dcf011afb3
Hicham Abdealtif <subzer0crack@gmail.com>
2020-02-15 03:02:48 +0100

    added the Category Model fixed some bugs

--- pages/models.py
+++ pages/models.py
@@ -2,33 +2,41 @@ from django.db import models
 from django.contrib.auth.models import User  
 from django.urls import reverse
 from django.utils import timezone
-#from django.utils.text import Slugfy
+from django.utils.text import slugify
 import uuid
 STATUS = (
     (0,"Draft"),
     (1,"Publish"),
 )
 # Create your models here.
+class Category (models.Model) : 
+    category = models.CharField(max_length = 32,null = False )
+
+    def __str__(self):
+        return self.category
+    
 class Post(models.Model):
     """Model definition for Post."""
    
     # TODO: Define fields here
     title = models.CharField(max_length=200)
-    slug=models.SlugField(max_length=250,default=uuid.uuid4,unique=True)
     content = models.TextField()
+    categories = models.ForeignKey(Category,default = 0,on_delete=models.DO_NOTHING)
     created_on = models.DateTimeField(default=timezone.now)
     updated_on=models.DateTimeField(auto_now=True)
     author = models.ForeignKey(User,on_delete=models.DO_NOTHING)
     status = models.IntegerField(choices=STATUS,default=0)
+    slug=models.SlugField(max_length=250,default=uuid.uuid4,unique=True)
 
     def __str__(self):
        return self.title + ' posted by |' + self.author.username
 
     def save(self,*args,**kwargs) : 
-        from django.utils.text import Slugfy
-        self.slug = Slugfy(self.title) 
+        
+        self.slug = slugify(self.title,allow_unicode=True) 
         return super(Post,self).save(*args,**kwargs)
 
     def get_absolute_url(self,*args,**kwargs):
-       
-        return reverse('home-page', kwargs={'slug': self.slug})
+        # added direct link in the admin page to the related post 
+        return reverse('post-detail', kwargs={'slug': self.slug})
+
